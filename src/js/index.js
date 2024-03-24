93a4e3a0a232642bc073231f40f5fae1510a5cdb
Sho.ayb <Sho.ayb@outlook.com>
2024-03-24 09:24:17 +0000

    Prettier error for trailing comma fixed with new rule included in eslintrc.json file

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,15 @@
-import "../scss/style.scss";
-import images from "../assets/img/logo.png";
+import '../scss/style.scss';
+import images from '../assets/img/logo.png';
 
-console.log("Webpack is serving!");
+console.log('Webpack is serving!');
 console.log(images);
+// console.log(
+//   `Testing if babel works by using a template literal because es5 does not support template literals.If babel is transpiling correctly than this statement will be in double quotes instead.`
+// );
+
+console.log(`I am testing if this template literal works with prettier`); // there was no error as the above with prettier
+
 console.log(
-  `Testing if babel works by using a template literal because es5 does not support template literals. If babel is transpiling correctly than this statement will be in double quotes instead.`,
+  `Testing if babel works by using a template literal because es5 does not support template literals. 
+  If babel is transpiling correct than this statement will in double quotes during build`
 );
